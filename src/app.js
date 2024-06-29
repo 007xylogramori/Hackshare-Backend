6d5a445ced11a4a79d41a4e52f1191d4e1a22f64
Himanshu <xylogramori@gmail.com>
2024-06-29 15:34:38 +0530

    user controller/model/routes added

--- src/app.js
+++ src/app.js
@@ -8,10 +8,18 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// Routes defined 
+
+import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+
 
 export { app }
